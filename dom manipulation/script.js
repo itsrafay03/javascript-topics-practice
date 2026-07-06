f7af467cff572e08122d796de21eb0c7ae78b2ce
itsrafay03 <itsrafay03@gmail.com>
2026-07-06 09:28:44 +0500

    DOM practice filees are added

--- dom manipulation/script.js
+++ dom manipulation/script.js
@@ -1,3 +1,35 @@
 console.log('Hello');
 // confirm("Do you want to visit this site.")
 // alert("Accept cookies.")
+
+// let inp = prompt("Hello");
+// console.log(inp);
+
+console.log("first")
+window.console.log("second")
+
+console.log(window.document);
+console.dir(window.document);
+
+console.log(document.body);
+console.dir(document.body);
+console.dir(document.body.childNodes[2]);
+
+console.log(document.body.childElementCount);
+console.log(document.body.childNodes);
+console.log(document.body.children);
+
+
+console.log(document.body.children[3].childElementCount);
+console.log(document.body.children[3].children);
+console.log(document.body.children[3].childNodes);
+
+console.log(document.body.children[4].childElementCount);
+console.log(document.body.children[4].childNodes);
+console.log(document.body.children[4].children);
+console.log(document.body.children[4].attributes);
+
+// document.body.children[2].innerText = "All is Well:";  
+// document.body.children[2].innerHTML = "All is Well:";  
+
+
